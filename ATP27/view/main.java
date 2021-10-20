9dfc3604ab29c9c7d4178cc27c0bd9f954a8bb90
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-20 17:26:06 -0300

    Correção

--- ATP27/view/main.java
+++ ATP27/view/main.java
@@ -10,6 +10,7 @@ public class main {
         FController fController = new FController();
         SController sController = new SController();
         Filmes f1 = new Filmes();
+        Filmes f2 = new Filmes();
         Series s1 = new Series();
 
         // FILMES 
@@ -17,16 +18,24 @@ public class main {
         f1.atriz_principal = "Jessica Tandy";
         f1.duracao = "2h 10m";
 
+        f2.nome_filme = "Spider-Man";
+        f2.atriz_principal = "Tobey";
+        f2.duracao = "2h 30m";
+
         // SÉRIES
         s1.genero = "Dorgas";
         s1.nome_serie = "Breaking Bad";
         s1.temporadas = 5;
 
         fController.create(f1);
-        System.out.println(f1);
+        fController.existe(f1);
+        fController.create(f2);
+        fController.delete(f1);
         sController.create(s1);
+        
+        System.out.println(f1);
+        System.out.println(f2);
         System.out.println(s1);
-
     }
 }
 
